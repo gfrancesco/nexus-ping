d94ade11ffe68661ec95c76a1492ca1b9c2291bf
Francesco Gianni <gfrancesco@gmail.com>
2013-01-19 18:33:17 +0100

    ADD: http headers to request

--- lib/nexus-ping/pinger.rb
+++ lib/nexus-ping/pinger.rb
@@ -19,8 +19,14 @@ module NexusPing
     private
 
     def check_stock device
-      http = Curl.get(@base_url + device)
-      
+      http = Curl::Easy.new(@base_url + device) do |curl|
+        curl.headers["Accept"] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+        curl.headers["Accept-Encoding"] = 'gzip, deflate'
+        curl.headers["Accept-Language"] = 'en-US,en;q=0.5'
+        curl.headers["Connection"] = 'keep-alive'
+        curl.headers["User-Agent"] = 'Mozilla/5.0 (X11; Linux x86_64; rv:18.0) Gecko/20100101 Firefox/18.0'
+      end
+      http.perform
       # Heuristic to check, in a language independent way, if 
       # 'not avaible in your country' page is returned
       raise InvalidIPError if http.body_str.size < 5000
